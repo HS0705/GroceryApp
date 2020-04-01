7bae2b45b25e594d657feaf619679d09bb00dcbd
HS0705 <hamidashaik@gmail.com>
2020-03-31 17:45:46 -0700

    first commit

---
+++ src/components/Product.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+export default class Product extends Component{
+    render() {
+        return(
+            <div>
+                <h3>
+                     Product 1
+                </h3>
+                <h3>
+                     Product 2
+                </h3>
+                <h3>
+                     Product 3
+                </h3>
+            </div>
+        )
+        
+    }
+}
