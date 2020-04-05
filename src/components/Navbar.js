b75e8e184cad819f23ed9bb90e766b839cebd63f
HS0705 <hamidashaik@gmail.com>
2020-04-05 13:30:12 -0700

    Navbar Updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,33 @@
 import React, {Component}  from 'react';
-
+import {Link} from 'react-router-dom';
+import logo from '../logo.png';
+import styled from 'styled-components';
+import {ButtonContainer} from './button';
 export default class Navbar extends Component{
 
     render() {
         return(
-            <div>
-                <h3>Hello from Navbar!!</h3>
-            </div>
-        )
+            <nav className="navbar navbar-exapnd-sm bg-primary navbar-dark px-sm-5">
+                <Link to='/'>
+                    <img src={logo} alt="store" className="navbar-brand" width="50" height="50"  />
+                </Link>
+                    <ul className="navbar-nav align-items-center">
+                        <li className="nav-item ml-5">
+                            <Link to ="/" className="nav-link">
+                               Grocery Items
+                            </Link>
+                        </li>
+                    </ul>
+                    <Link to="/cart" className="ml-auto">
+                        <ButtonContainer>
+                            <span className="mr-2">
+                                <i className="fas fa-cart-plus" />
+                            </span>                     
+                            my cart
+                        </ButtonContainer>
+                    </Link>                
+            </nav>
+        );
     }
 }
+
